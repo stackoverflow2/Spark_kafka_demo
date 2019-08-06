3ada4f6c02773c475c69e9d97ab1530238e8f1a4
stackoverflow2 <rameshwardhondge@gmail.com>
2019-08-06 11:10:54 +0530

    Debezuim parser are added

---
+++ src/main/java/debezium/ConsumerScheduler.java
@@ -0,0 +1,15 @@
+package debezium;
+
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
+
+
+public class ConsumerScheduler {
+
+
+}
